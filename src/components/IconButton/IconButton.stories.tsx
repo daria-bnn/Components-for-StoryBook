48f2b02e7476423eccc029281730ccc18b268c20
Daria <daria.bnn@bk.ru>
2022-07-08 14:21:06 +0300

    add-story-IconButton

---
+++ src/components/IconButton/IconButton.stories.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+
+import './IconButton.css'
+
+import IconButton from './IconButton'
+
+export default {
+  title: 'Icon-button',
+  component: IconButton,
+  argTypes: {
+    nameImg: {
+      name: 'Выберите иконку',
+      control: {
+        type: 'select',
+      },
+    },
+    size: {
+      name: 'Укажите размер икноки',
+    },
+    text: {
+      name: 'Введите любой текст',
+    },
+  },
+} as ComponentMeta<typeof IconButton>
+
+const Template: ComponentStory<typeof IconButton> = (args) => (
+  <IconButton {...args} />
+)
+
+export const CrabButton = Template.bind({})
+export const WhaleButton = Template.bind({})
+
+CrabButton.args = {
+  text: 'Крабо-кнопка',
+  size: 80,
+  nameImg: 'crab',
+}
+
+WhaleButton.args = {
+  text: 'Кнопка-кит',
+  size: 100,
+  nameImg: 'whale',
+}
